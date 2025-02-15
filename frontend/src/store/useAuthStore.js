f18a5e250f398db3f16d557e25f27d70876f43df
Ayush kumar Jha <ayushjha298@gmail.com>
2025-02-15 16:41:29 +0530

    minor changes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,14 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://alum-chatter-backend.onrender.com/";
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001"
+  : "https://alum-chatter-backend.onrender.com";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, 
+});
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
